85338c49d153e68a500bbd3337c7c94b2ff0b405
Jhen53 <milkcover53@gmail.com>
2025-12-29 23:58:01 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("active");
+  document.body.classList.toggle("menu-open");
 
   // 這一行是關鍵
   if (menu.classList.contains("active")) {
